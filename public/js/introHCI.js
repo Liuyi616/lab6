8f7b8bcf420af9b7e95413a2461ae553be36fb44
Liuyi616 <lion61ly@gmail.com>
2021-02-11 12:56:30 -0800

    2.11 edition

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	// var projectId_url = "project/" + idNumber;
+	$.get("project/" + idNumber,callback);
+	console.log("/project/" + idNumber);
 }
+	function callback(result){
+		console.log(result);
+		var select = "#project" + result.id + " .details";
+		var add_html = '<img src="'+ result.image + '"class="detailsImage">' + 
+		'<p>' + result.title + '</p>' +
+		'<p>' + result.date + '</p>' +
+		'<p>' + result.summary + '</p>';
+		$(select).html(add_html);
+
+	}
